3dc75b7244d99f8ce2147d4a8f784c077bc15f1b
liulingli <107923810@qq.com>
2018-02-12 17:46:23 +0800

    数独游戏开发练习中

---
+++ sudo/common.js
@@ -0,0 +1,7 @@
+/**
+ * 生成1-9的随机整数
+ * @return {}
+ */
+function generateRandom() {
+    return Math.floor(Math.random() * 9 + 1);
+}
---
+++ sudo/game.js
@@ -0,0 +1,4 @@
+/**
+ * Created by ADMINISTRATOR on DATE
+ * description description
+ */
